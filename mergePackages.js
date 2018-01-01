54c51a8350ddb5417fc0be88df73c1afebe53b46
Duarte David <deltaduartedavid@gmail.com>
2018-01-01 19:16:54 +0100

    Add typescript as dependency

--- mergePackages.js
+++ mergePackages.js
@@ -3,6 +3,9 @@ const { readFileSync, writeFileSync, createReadStream, createWriteStream } = req
 const currentPackage = JSON.parse(readFileSync('package.json'));
 const HTMLLSPackage = JSON.parse(readFileSync('vscode-html-languageserver/package.json'));
 
-writeFileSync('dist/package.json', JSON.stringify(Object.assign(HTMLLSPackage, currentPackage, { scripts: {} }), null, 2));
+writeFileSync('dist/package.json', JSON.stringify(Object.assign(HTMLLSPackage, currentPackage, {
+	scripts: {},
+	dependencies: Object.assign({}, HTMLLSPackage.dependencies, currentPackage.devDependencies)
+}), null, 2));
 createReadStream('README.md').pipe(createWriteStream('dist/README.md'));
 createReadStream('LICENSE').pipe(createWriteStream('dist/LICENSE'));
